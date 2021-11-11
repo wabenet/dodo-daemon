c75f2c40579b28aac88a2a5d399e3ace78537f8f
Ole Claussen <ole.claussen@wabenet.com>
2021-11-11 12:13:09 +0000

    refactor: remove the dependency to build logic from core

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/dodo-cli/dodo-daemon
 go 1.16
 
 require (
-	github.com/dodo-cli/dodo-core v0.2.0
+	github.com/dodo-cli/dodo-core v0.2.1-0.20211111091733-23d0e72df642
 	github.com/spf13/cobra v1.1.1
 )
--- pkg/command/run.go
+++ pkg/command/run.go
@@ -1,11 +1,14 @@
 package command
 
 import (
+	"fmt"
+
 	api "github.com/dodo-cli/dodo-core/api/v1alpha2"
-	"github.com/dodo-cli/dodo-core/pkg/core"
 	"github.com/dodo-cli/dodo-core/pkg/plugin"
+	"github.com/dodo-cli/dodo-core/pkg/plugin/builder"
 	"github.com/dodo-cli/dodo-core/pkg/plugin/configuration"
 	"github.com/dodo-cli/dodo-core/pkg/plugin/runtime"
+	"github.com/dodo-cli/dodo-core/pkg/ui"
 )
 
 func RunContainer(m plugin.Manager, name string) error {
@@ -13,7 +16,7 @@ func RunContainer(m plugin.Manager, name string) error {
 	config.ContainerName = config.Name
 
 	if len(config.ImageId) == 0 {
-		imageID, err := core.BuildByName(m, config.BuildInfo)
+		imageID, err := buildByName(m, config.BuildInfo)
 		if err != nil {
 			return err
 		}
@@ -79,4 +82,52 @@ func RestartContainer(m plugin.Manager, name string) error {
 	}
 
 	return rt.StartContainer(containerID)
+}
+
+func buildByName(m plugin.Manager, overrides *api.BuildInfo) (string, error) {
+	config, err := configuration.FindBuildConfig(m, overrides.ImageName, overrides)
+	if err != nil {
+		return "", fmt.Errorf("error finding build config for %s: %w", overrides.ImageName, err)
+	}
+
+	for _, dep := range config.Dependencies {
+		conf := &api.BuildInfo{}
+		configuration.MergeBuildInfo(conf, overrides)
+		conf.ImageName = dep
+
+		if _, err := buildByName(m, conf); err != nil {
+			return "", err
+		}
+	}
+
+	return buildImage(m, config)
+}
+
+func buildImage(m plugin.Manager, config *api.BuildInfo) (string, error) {
+	b, err := builder.GetByName(m, config.Builder)
+	if err != nil {
+		return "", fmt.Errorf("could not find build plugin for %s: %w", config.Builder, err)
+	}
+
+	imageID := ""
+
+	err = ui.NewTerminal().RunInRaw(
+		func(t *ui.Terminal) error {
+			if id, err := b.CreateImage(config, &plugin.StreamConfig{
+				Stdin:          t.Stdin,
+				Stdout:         t.Stdout,
+				Stderr:         t.Stderr,
+				TerminalHeight: t.Height,
+				TerminalWidth:  t.Width,
+			}); err != nil {
+				return fmt.Errorf("error in container I/O stream: %w", err)
+			} else {
+				imageID = id
+			}
+
+			return nil
+		},
+	)
+
+	return imageID, err
 }
